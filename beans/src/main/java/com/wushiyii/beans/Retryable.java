09f74995368ce1345ca2e8fefc8f797b0141d7cb
jessyy <wgq287676521@qq.com>
2021-06-18 11:34:12 +0800

    add mq-consumer module

--- beans/src/main/java/com/wushiyii/beans/Retryable.java
+++ beans/src/main/java/com/wushiyii/beans/Retryable.java
@@ -2,6 +2,7 @@ package com.wushiyii.beans;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 import java.util.function.Supplier;
 
@@ -27,15 +28,15 @@ public class Retryable<T> {
 
 
     public static <T> Retryable<T> of (Supplier<T> supplier) {
-        return of(supplier, DEFAULT_RETRY_MAX_COUNT, DEFAULT_RETRY_TIME);
+        return of(supplier, DEFAULT_RETRY_MAX_COUNT);
     }
 
     public static <T> Retryable<T> of (Supplier<T> supplier, int retryMaxCount) {
-        return of(supplier, retryMaxCount, DEFAULT_RETRY_TIME);
+        return of(supplier, retryMaxCount, DEFAULT_RETRY_TIME, TimeUnit.MICROSECONDS);
     }
 
-    public static <T> Retryable<T> of (Supplier<T> supplier, int retryMaxCount, long retryTime) {
-        return new Retryable<>(supplier, retryMaxCount, retryTime);
+    public static <T> Retryable<T> of (Supplier<T> supplier, int retryMaxCount, long retryTime, TimeUnit timeUnit) {
+        return new Retryable<>(supplier, retryMaxCount, timeUnit.toNanos(retryTime));
     }
 
     public void retry() {
--- beans/src/test/java/com/wushiyii/beans/RetryableTest.java
+++ beans/src/test/java/com/wushiyii/beans/RetryableTest.java
@@ -4,12 +4,13 @@ import com.wushiyii.BaseTest;
 import org.junit.Test;
 
 
+
 public class RetryableTest extends BaseTest {
 
     @Test
     public void testRetry() {
 
-        Retryable.of(() -> 0 / 10).retry();
+//        Retryable.of(() -> 0 / 10, 10, 1000).retry();
         Retryable.of(() -> 10 / 0).retry();
 
     }
